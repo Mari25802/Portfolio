ff59c2a46b035483b36d22c2c3862b28ccf7b447
Mariselvam <126177490+Mari25802@users.noreply.github.com>
2024-11-25 19:47:14 +0530

    second commit

--- scripts.js
+++ scripts.js
@@ -1,12 +1,5 @@
-// document.getElementById('download').addEventListener('click',()=>{
-//     const link=document.createElement('a');
-//     link.href="http://127.0.0.1:5500/resume.pdf"
-//     link.download="resume.pdf"
-//     link.click();
-// })
-
 document.getElementById('download').addEventListener('click',()=>{
-    const link=document.createElement("a")
+    const link=document.createElement('a');
     link.href="http://127.0.0.1:5500/resume.pdf"
     link.download="resume.pdf"
     link.click();
