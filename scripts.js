c0f4827a1ab9750cb1ba1e63fb863166fddef56b
Mariselvam <126177490+Mari25802@users.noreply.github.com>
2024-11-25 19:51:46 +0530

    second commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.getElementById('download').addEventListener('click',()=>{
     const link=document.createElement('a');
-    link.href="http://127.0.0.1:5500/resume.pdf"
+    link.href="https://mari25802.github.io/Portfolio/resume.pdf"
     link.download="resume.pdf"
     link.click();
 })
