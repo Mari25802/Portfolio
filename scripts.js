e73882f5772cc971879b03d662eb5ec7c8b4a146
Mariselvam <126177490+Mari25802@users.noreply.github.com>
2025-01-08 20:47:24 +0530

    first

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.getElementById('download').addEventListener('click',()=>{
     const link=document.createElement('a');
-    link.href="https://mari25802.github.io/Portfolio/resume.pdf"
-    link.download="resume.pdf"
+    link.href="https://mari25802.github.io/Portfolio/mariselvam_resume.pdf"
+    link.download="mariselvam_resume.pdf"
     link.click();
 })
